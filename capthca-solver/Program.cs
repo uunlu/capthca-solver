2f77dedd6fe7e909476d25fda834f1724c183420
ugur.unlu <vally10@live.com>
2016-09-04 16:59:59 -0400

    first commit with math library

--- capthca-solver/Program.cs
+++ capthca-solver/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,31 @@ namespace capthca_solver
     {
         static void Main(string[] args)
         {
+            float[][] array2D = new float[][]
+            { new float[]{ 1, 2 },
+              new float[]{ 3, 4 },
+              new float[]{ 5, 6 },
+              new float[]{ 7, 8 } };
+
+            //var matrix = new Matrix(array2D);
+
+            //Console.WriteLine(matrix);
+            //Console.WriteLine(MatrixUtilities.Transpose(matrix));
+
+            Matrix<double> matrix = DenseMatrix.OfArray(new double[,] {
+                {1,1,1,1},
+                {1,2,3,4},
+                {4,3,2,1}
+            });
+
+            Console.WriteLine(matrix);
+            Console.WriteLine(matrix.Transpose());
+
+            var matrix2 = matrix.Multiply(2);
+            Console.WriteLine(matrix2);
+
+            var matrix2matrix = matrix.Multiply(matrix.Transpose());
+            Console.WriteLine(matrix2matrix);
         }
     }
 }
